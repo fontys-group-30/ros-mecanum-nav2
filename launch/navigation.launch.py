885da17adc382ad29b1aa38b0f45d5470551583b
Reno Muijsenberg <490412@student.fontys.nl>
2024-10-09 12:37:06 +0200

    added correct map

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -8,13 +8,13 @@ from launch_ros.actions import Node
 def generate_launch_description():
     config_dir = "/home/reno/turtlebot3_ws/src/auto-navigation/config"
     param_file = os.path.join(config_dir, "tb3_nav_params.yaml")
-    map_file = os.path.join(config_dir, "tb3_map.yaml")
+    map_file = os.path.join(config_dir, "my_map.yaml")
     rviz_config_dir = os.path.join(config_dir, 'navigation.rviz')
 
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch', 'turtlebot3_world.launch.py')]),
-        ),
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch', 'turtlebot3_world.launch.py')]),
+        # ),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')]),
